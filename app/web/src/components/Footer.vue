88cbeac0405b5c4603edfadb6175186782915ee6
Kevin Harris <Kevin.Harris@microsoft.com>
2018-04-26 11:37:10 -0400

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team - 20180401
+        Azure Global Blackbelt Team - 20180425
       </div>
       <div class="col-lg-6">
       </div>
